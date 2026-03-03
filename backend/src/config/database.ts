a65d1f0fed66351b2b75fe2b216d83db327602ee
Lucas Oliverio <olucasoliverio@gmail.com>
2026-03-02 23:26:49 -0300

    fix: prisma env loading

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+
 
 export const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
