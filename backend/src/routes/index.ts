156a65061938de44676059b9c526773b125286eb
Lucas Oliverio Dos Santos <lucas.45324@alunosatc.edu.br>
2026-03-04 19:47:49 -0300

    auth route delete.1

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import authRoutes from './auth.routes';
 import conversationRoutes from './conversation.routes';
 import analysisRoutes from './analisys.routes';
 
 const router = Router();
 
-router.use('/auth', authRoutes);
 router.use('/conversations', conversationRoutes);
 router.use('/analyses', analysisRoutes);
 
